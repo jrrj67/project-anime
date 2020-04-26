3b297716ccb39b6c8b76b2301d8fa4e5bf5ade07
jrrj67 <ricardojunior.6767@gmail.com>
2020-04-26 20:01:40 -0300

    alterando link

--- resources/views/videos/watch.blade.php
+++ resources/views/videos/watch.blade.php
@@ -11,7 +11,7 @@
     <div class="container">
         <div class="row">
             <video class="responsive-video col l12" controls>
-                <source src="https://github.com/jrrj67/pa-server/raw/master/naruto-shippuden/{{$id}}.mp4" type="video/mp4">
+                <source src="https://www.googleapis.com/drive/v3/files/1rv7eQu9Q9IspYGzym_XWtvGJU5yqu3ln?alt=media&key=AIzaSyBQ9GUq5NPGdrdVJTpiWCWIZ3loLFzKNn4" type="video/mp4">
             </video>
         </div>
     </div>
