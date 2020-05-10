f58b34ffadad91065fdb3590c17d6e69d4be9118
jrrj67 <ricardojunior.6767@gmail.com>
2020-05-10 01:51:56 -0300

    Deploy

--- app/Http/Controllers/NarutoShippudenController.php
+++ app/Http/Controllers/NarutoShippudenController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Psy\Util\Str;
+use Illuminate\Support\Str;
 
 class NarutoShippudenController extends Controller
 {
